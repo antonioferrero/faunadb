95046adcd395760c9e274cfd8dde18fc4a0cfa3c
antonioferrero <franquitoferrero@hotmail.com>
2020-11-04 16:48:07 -0300

    api to .netlify functions

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -16,7 +16,7 @@ const LinkForm = ({ refreshLinks }) => {
     e.preventDefault()
     const body = { name, url, description }
     try {
-      await fetch('/api/createLink', {
+      await fetch('/.netlify/functions/createLink', {
         method: 'POST',
         body: JSON.stringify(body),
       })
